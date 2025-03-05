4a1abf4a64d56907415c2914610b35e144cbb8df
mustafa yucel <mustafaycl37@gmail.com>
2025-03-06 01:54:12 +0300

    New shapes added.

--- InventorAutomation/Form1.Designer.cs
+++ InventorAutomation/Form1.Designer.cs
@@ -3,11 +3,17 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.FlowLayoutPanel panelButtons;
         private System.Windows.Forms.Button btnStartInventor;
         private System.Windows.Forms.Button btnCreatePart;
-        private System.Windows.Forms.Button btnCreateSketch;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnPentagon;
+        private System.Windows.Forms.Button btnHexagon;
         private System.Windows.Forms.Button btnSaveAndClose;
-        private System.Windows.Forms.Label lblTitle;
 
         protected override void Dispose(bool disposing)
         {
@@ -21,64 +27,78 @@
         private void InitializeComponent()
         {
             this.Text = "Inventor Automation";
-            this.Size = new System.Drawing.Size(350, 350);
+            this.Size = new System.Drawing.Size(650, 850);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.BackColor = System.Drawing.Color.LightGray;
 
+            panelHeader = new System.Windows.Forms.Panel()
+            {
+                Dock = DockStyle.Top,
+                BackColor = System.Drawing.Color.DarkGray,
+                Padding = new Padding(0, 20, 0, 20),
+                AutoSize = true
+            };
+            this.Controls.Add(panelHeader);
+
             lblTitle = new System.Windows.Forms.Label()
             {
                 Text = "Autodesk Inventor Kontrol Paneli",
-                Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold),
-                AutoSize = true,
-                Location = new System.Drawing.Point(50, 20)
+                Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold),
+                AutoSize = false,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Top,
+                ForeColor = System.Drawing.Color.White
             };
-            this.Controls.Add(lblTitle);
+            panelHeader.Controls.Add(lblTitle);
 
-            btnStartInventor = new System.Windows.Forms.Button()
+            panelButtons = new System.Windows.Forms.FlowLayoutPanel()
             {
-                Text = "Autodesk Inventor Başlat",
-                Size = new System.Drawing.Size(250, 40),
-                Location = new System.Drawing.Point(50, 60),
-                BackColor = System.Drawing.Color.LightBlue,
-                Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold)
+                Dock = DockStyle.Fill,
+                FlowDirection = FlowDirection.TopDown,
+                Padding = new Padding(50, 100, 50, 20),
+                BackColor = System.Drawing.Color.WhiteSmoke,
+                AutoSize = false,
+                WrapContents = false
             };
-            btnStartInventor.Click += new System.EventHandler(this.btnStartInventor_Click);
-            this.Controls.Add(btnStartInventor);
+            this.Controls.Add(panelButtons);
 
-            btnCreatePart = new System.Windows.Forms.Button()
-            {
-                Text = "Yeni Parça Oluştur",
-                Size = new System.Drawing.Size(250, 40),
-                Location = new System.Drawing.Point(50, 110),
-                BackColor = System.Drawing.Color.LightGreen,
-                Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold)
-            };
-            btnCreatePart.Click += new System.EventHandler(this.btnCreatePart_Click);
-            this.Controls.Add(btnCreatePart);
+            btnStartInventor = AddButton("Autodesk Inventor Başlat", System.Drawing.Color.LightBlue, btnStartInventor_Click);
+            btnCreatePart = AddButton("Yeni Parça Oluştur", System.Drawing.Color.LightGreen, btnCreatePart_Click);
+            btnRectangle = AddButton("Dikdörtgen Çiz", System.Drawing.Color.LightGoldenrodYellow, btnRectangle_Click);
+            btnTriangle = AddButton("Üçgen Çiz", System.Drawing.Color.LightGoldenrodYellow, btnTriangle_Click);
+            btnCircle = AddButton("Daire Çiz", System.Drawing.Color.LightGoldenrodYellow, btnCircle_Click);
+            btnPentagon = AddButton("Beşgen Çiz", System.Drawing.Color.LightGoldenrodYellow, btnPentagon_Click);
+            btnHexagon = AddButton("Altıgen Çiz", System.Drawing.Color.LightGoldenrodYellow, btnHexagon_Click);
+            btnSaveAndClose = AddButton("Kaydet ve Kapat", System.Drawing.Color.LightCoral, btnSaveAndClose_Click);
 
-            btnCreateSketch = new System.Windows.Forms.Button()
-            {
-                Text = "Sketch Ekle",
-                Size = new System.Drawing.Size(250, 40),
-                Location = new System.Drawing.Point(50, 160),
-                BackColor = System.Drawing.Color.LightGoldenrodYellow,
-                Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold)
-            };
-            btnCreateSketch.Click += new System.EventHandler(this.btnCreateSketch_Click);
-            this.Controls.Add(btnCreateSketch);
+            SetInitialButtonStates();
+        }
 
-            btnSaveAndClose = new System.Windows.Forms.Button()
+        private Button AddButton(string text, System.Drawing.Color color, EventHandler clickEvent)
+        {
+            Button btn = new Button()
             {
-                Text = "Kaydet ve Kapat",
-                Size = new System.Drawing.Size(250, 40),
-                Location = new System.Drawing.Point(50, 210),
-                BackColor = System.Drawing.Color.LightCoral,
-                Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold)
+                Text = text,
+                AutoSize = false,
+                Size = new System.Drawing.Size(500, 60),
+                BackColor = color,
+                Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold),
+                Margin = new Padding(10),
+                FlatStyle = FlatStyle.Flat,
+                Enabled = false 
             };
-            btnSaveAndClose.Click += new System.EventHandler(this.btnSaveAndClose_Click);
-            this.Controls.Add(btnSaveAndClose);
+
+            btn.FlatAppearance.BorderSize = 0;
+            btn.Click += clickEvent;
+            panelButtons.Controls.Add(btn);
+            return btn;
+        }
+
+        private void SetInitialButtonStates()
+        {
+            btnStartInventor.Enabled = true;
         }
     }
 }
--- InventorAutomation/Form1.cs
+++ InventorAutomation/Form1.cs
@@ -17,6 +17,7 @@ namespace InventorAutomation
         {
             try
             {
+                btnCreatePart.Enabled = true;
                 Type inventorType = Type.GetTypeFromProgID("Inventor.Application");
                 _inventorApp = (Inventor.Application)Activator.CreateInstance(inventorType);
                 _inventorApp.Visible = true;
@@ -32,6 +33,13 @@ namespace InventorAutomation
         {
             try
             {
+                btnCreatePart.Enabled = false;
+                btnRectangle.Enabled = true;
+                btnTriangle.Enabled = true;
+                btnCircle.Enabled = true;
+                btnPentagon.Enabled = true;
+                btnHexagon.Enabled = true;
+                btnSaveAndClose.Enabled = true;
                 PartDocument partDoc = (PartDocument)_inventorApp.Documents.Add(DocumentTypeEnum.kPartDocumentObject,
                     _inventorApp.FileManager.GetTemplateFile(DocumentTypeEnum.kPartDocumentObject));
                 MessageBox.Show("Yeni parça oluþturuldu.");
@@ -41,8 +49,31 @@ namespace InventorAutomation
                 MessageBox.Show("Hata: " + ex.Message);
             }
         }
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            DrawShape(DrawRectangle, "Dikdörtgen çizildi.");
+        }
 
-        private void btnCreateSketch_Click(object sender, EventArgs e)
+        private void btnTriangle_Click(object sender, EventArgs e)
+        {
+            DrawShape(DrawTriangle, "Üçgen çizildi.");
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            DrawShape(DrawCircle, "Daire çizildi.");
+        }
+
+        private void btnPentagon_Click(object sender, EventArgs e)
+        {
+            DrawShape(DrawPentagon, "Beþgen çizildi.");
+        }
+
+        private void btnHexagon_Click(object sender, EventArgs e)
+        {
+            DrawShape(DrawHexagon, "Altýgen çizildi.");
+        }
+        private void DrawShape(Action<PlanarSketch> drawMethod, string message)
         {
             try
             {
@@ -50,31 +81,102 @@ namespace InventorAutomation
                 PartComponentDefinition compDef = partDoc.ComponentDefinition;
                 PlanarSketch sketch = compDef.Sketches.Add(compDef.WorkPlanes[3]); // XY Plane
 
-                TransientGeometry tg = _inventorApp.TransientGeometry;
+                drawMethod(sketch);
 
-                // Dikdörtgenin köþe noktalarý
-                SketchPoint p1 = sketch.SketchPoints.Add(tg.CreatePoint2d(0, 0), false);
-                SketchPoint p2 = sketch.SketchPoints.Add(tg.CreatePoint2d(10, 0), false);
-                SketchPoint p3 = sketch.SketchPoints.Add(tg.CreatePoint2d(10, 5), false);
-                SketchPoint p4 = sketch.SketchPoints.Add(tg.CreatePoint2d(0, 5), false);
-
-                // Kenarlarý oluþtur
-                sketch.SketchLines.AddByTwoPoints(p1, p2);
-                sketch.SketchLines.AddByTwoPoints(p2, p3);
-                sketch.SketchLines.AddByTwoPoints(p3, p4);
-                sketch.SketchLines.AddByTwoPoints(p4, p1);
-
-                MessageBox.Show("Sketch eklendi ve dikdörtgen çizildi.");
+                MessageBox.Show(message);
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Hata: " + ex.Message);
             }
         }
+
+        private void DrawRectangle(PlanarSketch sketch)
+        {
+            TransientGeometry tg = _inventorApp.TransientGeometry;
+
+            SketchPoint p1 = sketch.SketchPoints.Add(tg.CreatePoint2d(0, 0), false);
+            SketchPoint p2 = sketch.SketchPoints.Add(tg.CreatePoint2d(10, 0), false);
+            SketchPoint p3 = sketch.SketchPoints.Add(tg.CreatePoint2d(10, 5), false);
+            SketchPoint p4 = sketch.SketchPoints.Add(tg.CreatePoint2d(0, 5), false);
+
+            sketch.SketchLines.AddByTwoPoints(p1, p2);
+            sketch.SketchLines.AddByTwoPoints(p2, p3);
+            sketch.SketchLines.AddByTwoPoints(p3, p4);
+            sketch.SketchLines.AddByTwoPoints(p4, p1);
+        }
+
+        private void DrawTriangle(PlanarSketch sketch)
+        {
+            TransientGeometry tg = _inventorApp.TransientGeometry;
+
+            SketchPoint p1 = sketch.SketchPoints.Add(tg.CreatePoint2d(0, 0), false);
+            SketchPoint p2 = sketch.SketchPoints.Add(tg.CreatePoint2d(10, 0), false);
+            SketchPoint p3 = sketch.SketchPoints.Add(tg.CreatePoint2d(5, 8), false);
+
+            sketch.SketchLines.AddByTwoPoints(p1, p2);
+            sketch.SketchLines.AddByTwoPoints(p2, p3);
+            sketch.SketchLines.AddByTwoPoints(p3, p1);
+        }
+
+        private void DrawCircle(PlanarSketch sketch)
+        {
+            TransientGeometry tg = _inventorApp.TransientGeometry;
+            sketch.SketchCircles.AddByCenterRadius(tg.CreatePoint2d(5, 5), 5);
+        }
+
+        private void DrawPentagon(PlanarSketch sketch)
+        {
+            TransientGeometry tg = _inventorApp.TransientGeometry;
+            double centerX = 5, centerY = 5, radius = 4;
+            double angleStep = 2 * Math.PI / 5;
+
+            SketchPoint[] points = new SketchPoint[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                double x = centerX + radius * Math.Cos(i * angleStep);
+                double y = centerY + radius * Math.Sin(i * angleStep);
+                points[i] = sketch.SketchPoints.Add(tg.CreatePoint2d(x, y), false);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                sketch.SketchLines.AddByTwoPoints(points[i], points[(i + 1) % 5]);
+            }
+        }
+
+        private void DrawHexagon(PlanarSketch sketch)
+        {
+            TransientGeometry tg = _inventorApp.TransientGeometry;
+            double centerX = 5, centerY = 5, radius = 4;
+            double angleStep = 2 * Math.PI / 6;
+
+            SketchPoint[] points = new SketchPoint[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                double x = centerX + radius * Math.Cos(i * angleStep);
+                double y = centerY + radius * Math.Sin(i * angleStep);
+                points[i] = sketch.SketchPoints.Add(tg.CreatePoint2d(x, y), false);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                sketch.SketchLines.AddByTwoPoints(points[i], points[(i + 1) % 6]);
+            }
+        }
         private void btnSaveAndClose_Click(object sender, EventArgs e)
         {
             try
             {
+                btnCreatePart.Enabled = true;
+                btnRectangle.Enabled = false;
+                btnTriangle.Enabled = false;
+                btnCircle.Enabled = false;
+                btnPentagon.Enabled = false;
+                btnHexagon.Enabled = false;
+                btnSaveAndClose.Enabled = false;
                 PartDocument partDoc = (PartDocument)_inventorApp.ActiveDocument;
                 string directoryPath = "C:\\Temp\\";
                 if (!Directory.Exists(directoryPath))
